0c574a9e02de51a072627bd1dcf220a4735d78a0
Santiago Papa <papa.santiago321@gmail.com>
2025-04-14 20:30:03 -0300

    Possible fix

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "docs",
-  basePath: "/semantic",
-  assetPrefix: "/semantic",
   images: {
     unoptimized: true,
   },
