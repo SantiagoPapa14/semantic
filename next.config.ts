94c97599a7521f972b996386e9977113b06bfc39
Santiago Papa <papa.santiago321@gmail.com>
2025-04-14 20:07:48 -0300

    css build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "docs",
+  basePath: "/semantic",
+  assetPrefix: "/semantic/",
 };
 
 export default nextConfig;
