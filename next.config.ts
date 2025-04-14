cdcd57352380b8fec7666882a1cd1c88f03a9d84
Santiago Papa <papa.santiago321@gmail.com>
2025-04-14 20:18:33 -0300

    fix css locally

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "docs",
-  basePath: "/semantic",
-  assetPrefix: "/semantic/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
