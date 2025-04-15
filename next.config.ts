009fd088248ff4906f56646d726e78d1499d0ded
Santiago Papa <papa.santiago321@gmail.com>
2025-04-15 15:32:15 -0300

    asset prefix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-
 module.exports = {
   output: "export",
-  basePath: isGithubPages ? "/semantic" : "",
-  assetPrefix: isGithubPages ? "/semantic/" : "",
+  basePath: "/semantic",
+  assetPrefix: "/semantic/",
   images: {
     unoptimized: true,
   },
